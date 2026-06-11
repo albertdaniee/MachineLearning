d07509f1d10f6546eeb6a0fb48a4d67075fed3cc
Jayaraj Sunadararaj <47070500+albertdaniee@users.noreply.github.com>
2026-06-11 19:27:47 -0400

    Update Vite config to set base path
    Set base path for Vite configuration to '/MachineLearning/'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/MachineLearning/'
+})
 
 export default defineConfig(() => {
   return {
